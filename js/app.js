1a9f5ac370372cc0c3ea899dee08121e2939d443
Soulouf <soulouf@larasou.com>
2018-04-13 14:02:15 +0200

    Une liste de tâches

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        firstname: 'Jean',
-        lastname: 'Blabla',
+      tasks: [
+          { description: "Faire a manger", completed: false, icon: 'close icon' },
+          { description: "Faire les course", completed: true, icon: 'check icon' },
+          { description: "Premener le chien", completed: false, icon: 'close icon' },
+          { description: "Payer les factures", completed: true, icon: 'check icon' },
+          { description: "Faire mon site internet", completed: false, icon: 'close icon' },
+          { description: "Regarder la télé", completed: true, icon: 'check icon' },
+          { description: "Faire le ménage", completed: true, icon: 'check icon' },
+          { description: "Faore dodo", completed: false, icon: 'close icon' },
+      ],
     },
     computed: {
-      fullname() {
-          return this.firstname + ' ' + this.lastname;
+      completed() {
+          return this.tasks.filter((task) => {
+              return task.completed
+          });
+      },
+      incompleted() {
+          return this.tasks.filter(task => ! task.completed);
       }
     },
     methods: {
