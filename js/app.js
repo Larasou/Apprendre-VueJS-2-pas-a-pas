ca6a54db44c01191e68cf345dc9fb27ed2d5cd60
Soulouf <soulouf@larasou.com>
2018-04-18 20:56:52 +0200

    8- Les composants

--- js/app.js
+++ js/app.js
@@ -1,26 +1,17 @@
+Vue.component('list', {
+    template: `<li><slot></slot></li>`,
+    data() {
+        return {
+
+        }
+    }
+}),
+
 new Vue({
     el: '#app',
     data: {
-      tasks: [
-          { description: "Faire a manger", completed: false, icon: 'close icon' },
-          { description: "Faire les course", completed: true, icon: 'check icon' },
-          { description: "Premener le chien", completed: false, icon: 'close icon' },
-          { description: "Payer les factures", completed: true, icon: 'check icon' },
-          { description: "Faire mon site internet", completed: false, icon: 'close icon' },
-          { description: "Regarder la télé", completed: true, icon: 'check icon' },
-          { description: "Faire le ménage", completed: true, icon: 'check icon' },
-          { description: "Faore dodo", completed: false, icon: 'close icon' },
-      ],
     },
     computed: {
-      completed() {
-          return this.tasks.filter((task) => {
-              return task.completed
-          });
-      },
-      incompleted() {
-          return this.tasks.filter(task => ! task.completed);
-      }
     },
     methods: {
 
